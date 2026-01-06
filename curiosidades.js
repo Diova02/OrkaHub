8d1f44e76dbd0e65b7886e9296890704765ecdb1
Geovani Costa <geovanicosta730@gmail.com>
2026-01-06 12:59:42 -0300

    1.0.1 _ mais imagens e curiosidades

--- curiosidades.js
+++ curiosidades.js
@@ -1,24 +1,27 @@
 export default [
-    { 
-        dica: {
+    {   dica: {
             pt: "<strong>Cnidários</strong> são membros de um filo zoológico ao qual pertencem as águas-vivas e outros animais aquáticos simples, radiais e urticantes. Duvido que você sabia disso.",
             en: "<strong>Cnidarians</strong> are members of a zoological phylum that includes jellyfish and other simple, radial, and stinging aquatic animals. I bet you didn't know that."
-        },
-        img: "Agua Viva"
+        }, img: "Agua Viva"
     },
-    { 
-        dica: {
+    {   dica: {
             pt: "A <strong>Ecolocalização</strong> é um sentido biológico usado por morcegos e golfinhos para detectar a posição de objetos através da emissão (e reflexão) de ondas sonoras. São tipo um Demolidor (ou Professor Xavier) da natureza.",
             en: "<strong>Echolocation</strong> is a biological sense used by bats and dolphins to detect the position of objects through the emission (and reflection) of sound waves. They are basically the Daredevil of nature."
-        },
-        img: "Golfinho" 
+        }, img: "Golfinho" 
     },
-    { 
-        dica: {
+    {   dica: {
             pt: "Os <strong>Marsupiais</strong>, como cangurus e coalas, dão à luz filhotes subdesenvolvidos que terminam de crescer dentro de uma bolsa no corpo da mãe. Imagina se humanos tivessem essa feature!",
             en: "<strong>Marsupials</strong>, like kangaroos and koalas, give birth to undeveloped young that finish growing inside a pouch on the mother's body. Imagine if humans had this feature!"
-        },
-        img: "Canguru Vermelho" 
+        }, img: "Canguru Vermelho" 
+    },
+    {   dica: {
+            pt: "Sim. <strong>Isso é um animal</strong>. Prazer, esponja do mar!",
+            en: "Yes. <strong>That's an animal</strong>, Nice to meet you, sea sponge!"
+        }, img: "Esponja do mar" 
+    },
+    {   dica: {
+            pt: "A <strong>Lista vermelha da IUCN</strong> é uma lista que classifica os animais em riscos de extinção. Existem 9 categorias, indo de 'Pouco preocupante' até 'Extinto'. No Brasil, mais de 1.200 espécies de animais estão ameaçadas (incluindo as categorias Perigo Crítico, Perigo e Vulnerável)",
+            en: "The IUCN Red List is a list that classifies animals according to their risk of extinction. There are 9 categories, ranging from 'Least Concern' to 'Extinct'. In Brazil, more than 1,200 animal species are threatened (including the Critically Endangered, Endangered, and Vulnerable categories)."
+        }, img: "Mico leao dourado" 
     }
-    // Adicione mais dicas aqui...
 ];
